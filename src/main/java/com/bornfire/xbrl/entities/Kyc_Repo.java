b1b4891f62fa9deb2ad7fa8c9bdb75cb5fa09553
riyas-bornfire <mohamedriyas.a@bornfire.in>
2024-11-06 12:24:09 +0530

    DATA MAPED WITH THE INDIVIDUAL FORM

--- src/main/java/com/bornfire/xbrl/entities/Kyc_Repo.java
+++ src/main/java/com/bornfire/xbrl/entities/Kyc_Repo.java
@@ -9,9 +9,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface Kyc_Repo extends JpaRepository<KYC_I, String> {
 	@Query(value = "select * from KYC_IND where CUSTOMER_ID = ?1 ", nativeQuery = true)
-	List <KYC_I> GetUser(String custid);
+	List<KYC_I> GetUser(String custid);
 
-	@Query(value = "SELECT customer_id, name,account_number,ACCOUNT_TYPE,ACCOUNT_OPENING_DATE,MODIFY_FLG FROM kyc_ind WHERE del_flg = 'N'", nativeQuery = true)
+	@Query(value = "SELECT customer_id, name, account_number, ACCOUNT_TYPE, ACCOUNT_OPENING_DATE, MODIFY_FLG FROM kyc_ind WHERE del_flg = 'N'", nativeQuery = true)
 	List<Object[]> Getlist();
 
 }
+	
