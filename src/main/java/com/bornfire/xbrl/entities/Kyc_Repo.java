b3bc9ad5a42bb04ef9370228494f11895ee95913
riyas-bornfire <mohamedriyas.a@bornfire.co.in>
2024-12-24 10:31:23 +0530

    changes made to rectify the error

--- src/main/java/com/bornfire/xbrl/entities/Kyc_Repo.java
+++ src/main/java/com/bornfire/xbrl/entities/Kyc_Repo.java
@@ -24,7 +24,7 @@ public interface Kyc_Repo extends JpaRepository<KYC_I, String> {
 	@Query(value = "SELECT customer_id, name, account_number, ACCOUNT_TYPE, ACCOUNT_OPENING_DATE, MODIFY_FLG, "
 			+ "COUNTRY_OF_CITIZENSHIP, COUNTRY_OF_CURRENT_RESIDENCY, "
 			+ "FLOOR(MONTHS_BETWEEN(CURRENT_DATE, DATE_OF_BIRTH) / 12) AS age, CUSTOMER_RISK " + "FROM kyc_ind "
-			+ "WHERE CUSTOMER_RISK = ?1 AND FLOORFLOOR( REPORT_DATE - CURRENT_DATE) = ?2 "
+			+ "WHERE CUSTOMER_RISK = ?1 AND FLOOR( REPORT_DATE - CURRENT_DATE) = ?2 "
 			+ "AND del_flg = 'N'", nativeQuery = true)
 	List<Object[]> GetDynamicValue(String customerRisk, Integer age);
 
